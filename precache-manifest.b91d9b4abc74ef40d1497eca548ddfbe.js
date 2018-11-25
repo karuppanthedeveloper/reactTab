4cd91bc6396f5dc14f2d2cd02331738005520694
karuppusamy.p <pranav.karuppusamy@gmail.com>
2018-11-25 14:59:19 +0530

    Updates

---
+++ precache-manifest.b91d9b4abc74ef40d1497eca548ddfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTab/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f88cd1c81af47050c640",
+    "url": "/reactTab/static/js/runtime~main.f88cd1c8.js"
+  },
+  {
+    "revision": "db8f6a792d31390c3ee7",
+    "url": "/reactTab/static/js/main.db8f6a79.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/reactTab/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "db8f6a792d31390c3ee7",
+    "url": "/reactTab/static/css/main.90bf6f03.chunk.css"
+  },
+  {
+    "revision": "543c22e6e0f5abaea17c917199ecd723",
+    "url": "/reactTab/index.html"
+  }
+];
